00f865b7efe8b90b7701e59d0389e15c2a0c3f2c
Ankita Das <daszankita23@gmail.com>
2026-07-02 21:53:25 +0530

    Responsive navbar

---
+++ ResponsiveNavbar/src/App.jsx
@@ -0,0 +1,14 @@
+import { HeroSection } from "./Components/HeroSection";
+import { Navbar } from "./Components/NavBar";
+import "./App.css"
+
+const App = () =>{
+  return(
+    <>
+    <Navbar/>
+    <HeroSection/>
+     
+    </>
+  )
+}
+export default App;
---
+++ ResponsiveNavbar/src/Components/NavBar.jsx
@@ -0,0 +1,31 @@
+export const Navbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="logo">
+            <h1>Ankita</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+
+            </ul>
+          </nav>
+
+        </div>
+      </div>
+    </header>
+  );
+};

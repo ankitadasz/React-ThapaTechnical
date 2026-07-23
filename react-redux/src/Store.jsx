4da93ddcb0cebcb379f2e85222116ede1a92e548
Ankita Das <daszankita23@gmail.com>
2026-07-24 00:31:22 +0530

    create slice in redux toolkit

--- react-redux/src/Store.jsx
+++ react-redux/src/Store.jsx
@@ -1,61 +1,76 @@
 // import { applyMiddleware, createStore } from "redux";
 // import { composeWithDevTools } from "@redux-devtools/extension";
 // import { thunk } from "redux-thunk";
-import { configureStore } from "@reduxjs/toolkit";
-const ADD_TASK = "task/add";
-const DELETE_TASK = "task/delete";
-const FETCH_TASK = "task/fetch";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+// const ADD_TASK = "task/add";
+// const DELETE_TASK = "task/delete";
+// const FETCH_TASK = "task/fetch";
 const initialState = {
   task: [],
 };
 
-export const taskReducer = (state = initialState, action) => {
-  switch (action.type) {
-    ///case for adding task///
-    case ADD_TASK:
-      return {
-        ...state,
-        task: [...state.task, action.payload],
-      };
-    ///case for deleting task///
-    case DELETE_TASK:
-      const updatedTask = state.task.filter((currTask, index) => {
-        return index !== action.payload;
-      });
-      return {
-        ...state,
-        task: updatedTask,
-      };
-    ///case for fetching task///
-    case FETCH_TASK:
-      return {
-        ...state,
-        task: [...state.task, ...action.payload],
-      };
-    default:
-      return state;
-  }
-};
-export const addTask = (data) => {
-  return { type: ADD_TASK, payload: data };
-};
+// export const taskReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     ///case for adding task///
+//     case ADD_TASK:
+//       return {
+//         ...state,
+//         task: [...state.task, action.payload],
+//       };
+//     ///case for deleting task///
+//     case DELETE_TASK:
+//       const updatedTask = state.task.filter((currTask, index) => {
+//         return index !== action.payload;
+//       });
+//       return {
+//         ...state,
+//         task: updatedTask,
+//       };
+//     ///case for fetching task///
+//     case FETCH_TASK:
+//       return {
+//         ...state,
+//         task: [...state.task, ...action.payload],
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-export const deleteTask = (id) => {
-  return { type: DELETE_TASK, payload: id };
-};
-//!oldStyle
+//This belows are actioncreator
+// export const addTask = (data) => {
+//   return { type: ADD_TASK, payload: data };
+// };
+
+// export const deleteTask = (id) => {
+//   return { type: DELETE_TASK, payload: id };
+// };
+
+// //!oldStyle
 // export const store = createStore(
 //   taskReducer,
 //   composeWithDevTools(applyMiddleware(thunk))
 // );
 // console.log(store);
 
+//? RTK slice
+const taskReducer=createSlice({
+  name: "task",
+  initialState: initialState,
+  reducers: {
+    addTask(state, action) {},
+    deleteTask(state, action) {},
+  },
+});
+console.log(taskReducer)
+const {addTask,deleteTask} = taskReducer.actions;
+
 //!newStyle
-export const store=configureStore({
-  reducer:{
-    taskReducer:taskReducer,
-  }
-})
+export const store = configureStore({
+  reducer: {
+    taskReducer: taskReducer, //or u can write simply taskReducer instead of taskReducer:taskReducer
+  },
+});
 console.log(store.getState());
 console.log("InitialState:", store.getState());
 store.dispatch(addTask("Hello Ankita Pretty gurll"));
@@ -80,10 +95,12 @@ export const fetchTask = (id) => {
       );
       const task = await res.json();
       console.log(task);
-      dispatch({ type: FETCH_TASK, payload: task.map((currTask)=>currTask.title) });
-    } 
-    catch (error) {
-      console.log(error)
+      dispatch({
+        type: FETCH_TASK,
+        payload: task.map((currTask) => currTask.title),
+      });
+    } catch (error) {
+      console.log(error);
     }
   };
 };
